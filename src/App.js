fff8b348bc94c8aeee36361bb63a334eb238027e
Rafa Moreno <rmh1516@gmail.com>
2020-07-14 11:55:00 +0200

    refactor routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Layout from 'components/Layout'
-import Router from 'routes/Router'
+import routes from 'routes/config'
 
 function App() {
   return (
-    <Layout>
-      <Router />
-    </Layout>
+    <Router>
+      <Layout>
+        <Switch>
+          {routes.map(({ Page, path }) => (
+            <Route key={path} exact path={path}>
+              <Page />
+            </Route>
+          ))}
+        </Switch>
+      </Layout>
+    </Router>
   )
 }
 
