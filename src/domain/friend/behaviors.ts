bd57d417c0263abb66da6e39e8ca4350a52def6c
Rafa Moreno <rmh1516@gmail.com>
2020-10-20 18:20:09 +0200

    refactor friend behaviors

--- src/domain/friend/behaviors.ts
+++ src/domain/friend/behaviors.ts
@@ -2,22 +2,30 @@ import { UpdateFriendResult } from './types'
 import { User } from 'domain/user/types'
 import { updateItemFromArray } from 'utils/helpers'
 
+function addFriend(newFriend: User, friends: User[]): User[] {
+  return [...friends, newFriend]
+}
+
+function removeFriend(newFriend: User, friends: User[]): User[] {
+  return friends.filter(({ id }) => id !== newFriend.id)
+}
+
+function isFriend(user: User): boolean {
+  return !!user.followedAt
+}
 export function updateFriends(
   user: User,
   friends: User[],
   usersToFollow: User[]
 ): UpdateFriendResult {
-  if (!user || !friends) {
+  if (!user || !friends || !usersToFollow) {
     throw new Error('the data is not valid')
   }
-  let updatedFriends = friends
-  let updatedUsersToFollow = usersToFollow
-  if (user?.followedAt) {
-    updatedFriends = [...updatedFriends, user]
-  } else {
-    updatedFriends = updatedFriends.filter(({ id }) => id !== user.id)
-  }
-  updatedUsersToFollow = updateItemFromArray(updatedUsersToFollow, user)
+  const updatedFriends = isFriend(user)
+    ? addFriend(user, friends)
+    : removeFriend(user, friends)
+
+  const updatedUsersToFollow = updateItemFromArray(usersToFollow, user)
   return {
     friends: updatedFriends,
     usersToFollow: updatedUsersToFollow,
